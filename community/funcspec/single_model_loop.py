cf1b63b26da2a5fbeb66249f0e23ae2150667dbc
Gabriel Bena <gabriel.bena1@gmail.com>
2022-09-27 16:17:11 +0100

    metrics at multiple timesteps

--- community/funcspec/single_model_loop.py
+++ community/funcspec/single_model_loop.py
@@ -106,7 +106,7 @@ def compute_all_metrics(trained_coms, loaders, config, device) :
     bottleneck_results = readout_retrain(community, loaders, n_classes, deepR_params_dict=deepR_params_dict, n_tests=1, n_epochs=3, device=device, use_tqdm=1, symbols=symbols)
     bottleneck_metric = bottleneck_results['accs'].mean(0)
 
-    diff_metric = lambda metric, ag : (metric[ag, ag] - metric[1-ag, ag]) / (metric[ag, ag] + metric[1-ag, ag])
+    diff_metric = lambda metric : np.abs(metric[0] - metric[1]) / (metric[0] + metric[1])
 
     # ------ Log ------
     #metrics = [correlations_metric, masks_metric, bottleneck_metric]
@@ -123,10 +123,12 @@ def compute_all_metrics(trained_coms, loaders, config, device) :
     for metric, metric_name in zip(metrics, metric_names) : 
         
         metric_log = {}
-        for ag in range(2) : 
-            metric_log[f'{metric_name} Diff {ag}'] = diff_metric(metric, ag)
-            for task in range(2) : 
-                metric_log[f'{metric_name} Agent {ag}, Task {task}'] = metric[task, ag]
+        for s, step in enumerate(['Pre-Comms', 'Post-Comms']) : 
+            for ag in range(2) : 
+                single_metric = metric[:, ag, s]
+                metric_log[f'{metric_name} Diff {ag} {step}'] = diff_metric(single_metric)
+                for task in range(2) : 
+                    metric_log[f'{metric_name} Agent {ag}, Task {task} {step}'] = single_metric[task, ag]
         
         wandb.log(metric_log)
 
