5d5e9025761362d61f2e2d56f23ce7c1b9171cbe
Gabriel Bena <gabriel.bena1@gmail.com>
2022-10-12 11:01:28 +0100

    black format

--- community/data/tasks.py
+++ community/data/tasks.py
@@ -3,169 +3,203 @@ import torch
 import torchvision.transforms.functional as TF
 
 
-def get_digits(target, n_classes=10) : 
+def get_digits(target, n_classes=10):
 
     return target[..., 0], target[..., 1]
 
-def rotation_conflict_task(datas, digits, n_angles=4) : 
+
+def rotation_conflict_task(datas, digits, n_angles=4):
     device = datas.device
     digits = digits.to(device)
-    #Use on data before flatening/creating time_steps !
-    possible_angles = np.linspace(0, 360*(1 - 1/n_angles), n_angles, dtype=int)
+    # Use on data before flatening/creating time_steps !
+    possible_angles = np.linspace(0, 360 * (1 - 1 / n_angles), n_angles, dtype=int)
 
     datas_single_t = datas[:, 0], datas[:, 1]
-    angle_values = [np.random.choice(possible_angles, datas.shape[0]) for data in datas_single_t]
-    diff_choice = lambda i : np.random.choice(possible_angles[possible_angles != i])
+    angle_values = [
+        np.random.choice(possible_angles, datas.shape[0]) for data in datas_single_t
+    ]
+    diff_choice = lambda i: np.random.choice(possible_angles[possible_angles != i])
     v_choice = np.vectorize(diff_choice)
     angle_values[1] = v_choice(angle_values[0])
-    angle_values = torch.stack([torch.FloatTensor(a).to(device) for a in angle_values], 0).int()
+    angle_values = torch.stack(
+        [torch.FloatTensor(a).to(device) for a in angle_values], 0
+    ).int()
 
-    rotation = lambda d, angle : TF.rotate(d.unsqueeze(0), angle.data.item())
+    rotation = lambda d, angle: TF.rotate(d.unsqueeze(0), angle.data.item())
 
-    rotated_datas = torch.cat([torch.stack([rotation(d, a) for d, a in zip(data, angle)]) for data, angle in zip(datas_single_t, angle_values)], 1)
+    rotated_datas = torch.cat(
+        [
+            torch.stack([rotation(d, a) for d, a in zip(data, angle)])
+            for data, angle in zip(datas_single_t, angle_values)
+        ],
+        1,
+    )
     mask = (angle_values[0] < angle_values[1]).to(device)
     target = torch.where(mask, digits[:, 0], digits[:, 1])
     return rotated_datas, target, angle_values
 
-def get_task_target(target, task='parity_digits_10', temporal_target=False) : 
+
+def get_task_target(target, task="parity_digits_10", temporal_target=False):
     """
     Returns target for different possible tasks
-    Args : 
+    Args :
         targets : original digits : size (batch x 2)
-        task : task to be conducted : 
+        task : task to be conducted :
                digit number ("0", "1"), "parity", "parity_digits_10", "parity_digits_100" or "sum" ...
     """
 
     n_classes = len(target.unique())
 
-    if temporal_target : 
+    if temporal_target:
 
         return get_task_target(target, task, n_classes, False).unique(dim=0)
 
-    else : 
+    else:
 
         new_target = None
 
-        #Task can be a combination of subtasks, separated by _
-        tasks = task.split('_')
+        # Task can be a combination of subtasks, separated by _
+        tasks = task.split("_")
 
-        if 'inv' in tasks : 
+        if "inv" in tasks:
             new_target = target.flip(-1)
             digits = digits_0, digits_1 = get_digits(new_target, n_classes)
-        else : 
+        else:
             digits = digits_0, digits_1 = get_digits(target, n_classes)
 
-        parity = (digits_0 + digits_1)%2  #0 when same parity
-        target_100 = (digits_0*10 + digits_1)
+        parity = (digits_0 + digits_1) % 2  # 0 when same parity
+        target_100 = digits_0 * 10 + digits_1
 
-        if 'parity' in tasks : 
-            if 'digits' in tasks : 
+        if "parity" in tasks:
+            if "digits" in tasks:
                 new_target = torch.where(parity.bool(), digits_0, digits_1)
-            elif 'both' in tasks : 
-                new_target = torch.stack(torch.where(parity.bool(), digits_0, digits_1), torch.where(parity.bool(), digits_1, digits_0))
-            else : 
+            elif "both" in tasks:
+                new_target = torch.stack(
+                    torch.where(parity.bool(), digits_0, digits_1),
+                    torch.where(parity.bool(), digits_1, digits_0),
+                )
+            else:
                 new_target = parity
 
-        elif '100_class' in tasks: 
+        elif "100_class" in tasks:
             new_target = target_100
 
-        elif 'count' in tasks : 
+        elif "count" in tasks:
 
-            if 'max' in tasks : 
-                new_target = torch.where(target.argmax(-1).bool(), target[:, 1], target[:, 0])
+            if "max" in tasks:
+                new_target = torch.where(
+                    target.argmax(-1).bool(), target[:, 1], target[:, 0]
+                )
                 new_target[target[:, 0] == target[:, 1]] = 0
-            elif 'min' in tasks : 
-                new_target = torch.where(target.argmin(-1).bool(), target[:, 1], target[:, 0])
+            elif "min" in tasks:
+                new_target = torch.where(
+                    target.argmin(-1).bool(), target[:, 1], target[:, 0]
+                )
                 new_target[target[:, 0] == target[:, 1]] = 3
-            elif 'equal' in tasks : 
+            elif "equal" in tasks:
                 new_target = target[:, 0] != target[:, 1]
 
-        elif task == 'none' or task == 'both' : 
+        elif task == "none" or task == "both":
             new_target = target
 
-        if 'opposite' in tasks : 
-            if new_target is not None : 
+        if "opposite" in tasks:
+            if new_target is not None:
                 new_target = n_classes - new_target - 1
-            else: 
+            else:
                 new_target = n_classes - target - 1
 
-        if 'sum' in tasks : 
-            if new_target is not None : 
+        if "sum" in tasks:
+            if new_target is not None:
                 new_target = new_target.sum(-1)
-            else: 
+            else:
                 new_target = target.sum(-1)
 
-        try : 
+        try:
             task = int(tasks[-1])
-            if new_target is None : 
+            if new_target is None:
                 new_target = target
-            new_target = new_target[..., task] 
-            
-        except ValueError : 
-            'continue'
-            
-        if new_target is None : 
-            raise ValueError('Task recognized, try digit number ("0", "1"), "parity", "parity_digits", "parity_digits_100" or "sum" ')
+            new_target = new_target[..., task]
 
-        if len(new_target.shape) == 2 : 
+        except ValueError:
+            "continue"
+
+        if new_target is None:
+            raise ValueError(
+                'Task recognized, try digit number ("0", "1"), "parity", "parity_digits", "parity_digits_100" or "sum" '
+            )
+
+        if len(new_target.shape) == 2:
             new_target = new_target.T
-                    
-        return new_target
-#------ Continual Learning Tasks ------ : 
 
-def get_continual_task(data, target, task, seed, n_tasks=None, n_classes=10) : 
+        return new_target
+
+
+# ------ Continual Learning Tasks ------ :
+
+
+def get_continual_task(data, target, task, seed, n_tasks=None, n_classes=10):
 
     done = False
-    if seed == 'skip' or task == 'none' or task.split('_')[-1] == 'schedule': 
+
+    if seed == "skip" or task == "none" or task.split("_")[-1] == "schedule":
         torch.manual_seed(0)
         return data, target
-    else : 
+    else:
         seed = int(seed)
-    torch.manual_seed(seed)        
-    conv_data = (data.shape[-1] == data.shape[-2])
+    torch.manual_seed(seed)
+    conv_data = data.shape[-1] == data.shape[-2]
 
-    if  'label_perm' in task  : 
-        perm = torch.randperm(n_classes)if seed != 0 else torch.arange(n_classes)
-        permute = lambda x : perm[x]
+    if "label_perm" in task:
+        perm = torch.randperm(n_classes) if seed != 0 else torch.arange(n_classes)
+        permute = lambda x: perm[x]
         target = target.clone().cpu().apply_(permute).to(data.device)
         done = True
 
-    if 'pixel_perm' in task: 
+    if "pixel_perm" in task:
         flat = data
         n_pixels = flat.shape[-1]
-        perm = torch.randperm(n_pixels)if seed != 0 else torch.arange(n_pixels)
+        perm = torch.randperm(n_pixels) if seed != 0 else torch.arange(n_pixels)
         flat = flat[..., perm]
-        if conv_data : flat = flat[..., perm, :]
+        if conv_data:
+            flat = flat[..., perm, :]
         data = flat.view_as(data)
         done = True
-        
-    if 'rotate' in task :
-        if n_tasks is not None : 
-            angle = 360//n_tasks
-        else : 
+
+    if "rotate" in task:
+        if n_tasks is not None:
+            angle = 360 // n_tasks
+        else:
             angle = 10
-        data = torch.stack([TF.rotate(d, seed*angle) for d in data], 0)      
+        data = torch.stack([TF.rotate(d, seed * angle) for d in data], 0)
         done = True
 
-    if 'select' in task : 
-        target, _ = get_task_target(target, task=seed%2)
+    if "select" in task:
+        target, _ = get_task_target(target, task=seed % 2)
         done = True
 
-    if 'sequential' in task : 
-        if data.shape[1] != 10 and len(data.shape) != 5 : 
-            if len(data.shape) > 4 : 
-                data, target = data[:, target==seed%n_classes], target[target==seed%n_classes]
-            else : 
-                data, target = data[target==seed%n_classes], target[target==seed%n_classes]
-        else : 
-             data, target = data[:, seed%n_classes], target[:, seed%n_classes]
-             
-        done=True
+    if "sequential" in task:
+        if data.shape[1] != 10 and len(data.shape) != 5:
+            if len(data.shape) > 4:
+                data, target = (
+                    data[:, target == seed % n_classes],
+                    target[target == seed % n_classes],
+                )
+            else:
+                data, target = (
+                    data[target == seed % n_classes],
+                    target[target == seed % n_classes],
+                )
+        else:
+            data, target = data[:, seed % n_classes], target[:, seed % n_classes]
+
+        done = True
 
         assert data.shape[0] > 0, "Empty tensor returned"
 
-    if not done :
-        raise ValueError('Continual task setting not recognized !! Try "pixel_perm", "label_perm", or "rotate"')
+    if not done:
+        raise ValueError(
+            'Continual task setting not recognized !! Try "pixel_perm", "label_perm", or "rotate"'
+        )
 
     torch.manual_seed(torch.random.seed())
     return data, target
